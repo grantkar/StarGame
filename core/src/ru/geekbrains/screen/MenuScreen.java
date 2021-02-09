8e0488780749c5d079f0c31b1bd26eb558f4efe5
grantkar <hrant87@inbox.ru>
2021-02-09 13:50:54 +0400

    Каркас игры

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -6,28 +6,25 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.scenes.scene2d.actions.Actions;
-import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
+
 
 import ru.geekbrains.base.BaseScreen;
+import ru.geekbrains.math.Rect;
+import ru.geekbrains.sprite.Background;
 
 public class MenuScreen extends BaseScreen {
-   private Texture img;
-   private Vector2 touch;
-   private Vector2 v;
-   private Vector2 pos;
-   private Vector2 buff;
+   private Texture bg;
+   private Background background;
+
 
 
 
     @Override
     public void show() {
         super.show();
-        img = new Texture("SuperCat.jpg");
-        touch = new Vector2();
-        v = new Vector2(1,1);
-        pos = new Vector2(0,0);
-        buff = new Vector2();
+       bg = new Texture("texture/bg.png");
+       background = new Background(bg);
+
 
     }
 
@@ -36,30 +33,27 @@ public class MenuScreen extends BaseScreen {
         Gdx.gl.glClearColor(0.7f, 0.1f, 0.5f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(img, pos.x , pos.y);
+        background.draw(batch);
         batch.end();
 
-        buff = touch.sub(pos); // высчитываем вектор направление до точки нажатия мыши и сохраняем в буферный вектор
-        if (pos.x!=buff.x){   // теперь картинка двигается только после нашего нажатия
-            pos.add(v);
-        }
+
 
 
     }
 
     @Override
     public void dispose() {
-        img.dispose();
+        bg.dispose();
         super.dispose();
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        touch.set(screenX,Gdx.graphics.getHeight() - screenY);
-        pos.x = touch.x;
-        pos.y = touch.y;
-        return super.touchDown(screenX, screenY, pointer, button);
-
+    public void resize(Rect worldBounds) {
+        background.resize(worldBounds);
     }
 
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
+        return super.touchDown(touch, pointer, button);
+    }
 }
